cf19d50d6bb2ce4cf5724840ae5052f508f07632
Tyler7x1 <jpm022504@gmail.com>
2025-05-02 22:50:30 +0530

    Constructors with Struct

--- simple_struct/src/main.rs
+++ simple_struct/src/main.rs
@@ -5,6 +5,15 @@ struct Book {
     read: bool,
 }
 
+impl Book {
+    fn calculate_cost(&self) -> f64 {
+        self.pages as f64 * 0.90
+    }
+    fn compare_pages(&self, other: &Book) -> bool {
+        self.pages > other.pages
+    }
+}
+
 fn main() {
     let book1: Book = Book {
         title: String::from("The Rust Book"),
@@ -20,7 +29,12 @@ fn main() {
         read: false,
     };
     print_summary(&book1);
+    println!("Publishing Cost: $ {:.2}", book1.calculate_cost());
     print_summary(&book2);
+    println!("Publishing Cost: $ {:.2}", book2.calculate_cost());
+    let result: bool = book1.compare_pages(&book2);
+    if result { print!("book1 has more pages than book2."); } else { print!("book2 has more pages than book1."); }
+    println!()
 }
 
 fn print_summary(book: &Book) {
